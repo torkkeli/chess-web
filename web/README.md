86769c672cb12e8a201bb2b0a11d43e13c4d0241
Ted Torkkeli <tedtorkkeli@gmail.com>
2025-07-28 21:28:36 +0200

    base

---
+++ web/README.md
@@ -0,0 +1,19 @@
+# web
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Run Unit Tests with [Vitest](https://vitest.dev/)
+
+```sh
+npm run test:unit
+```
---
+++ web/src/components/ChessBoard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const rows = 8
+const cols = 8
+
+function getSquareColor(row: number, col: number) {
+  return (row + col) % 2 === 0 ? 'white' : 'black'
+}
+</script>
+
+<template>
+  <div class="chess-board">
+    <div
+      v-for="row in rows"
+      :key="row"
+      class="chess-row"
+    >
+      <div
+        v-for="col in cols"
+        :key="col"
+        class="chess-square"
+        :class="getSquareColor(row - 1, col - 1)"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.chess-board {
+  display: inline-block;
+  border: 2px solid #333;
+}
+.chess-row {
+  display: flex;
+}
+.chess-square {
+  width: 40px;
+  height: 40px;
+}
+.chess-square.white {
+  background: #e6cfa7;
+}
+.chess-square.black {
+  background: #4a2f11;
+}
+</style>
